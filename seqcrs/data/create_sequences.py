2a5d7ef15f229ea4c0054c6021728eb2fae9b701
Jose Hernandez <jmhernandeze@gmail.com>
2021-02-01 13:33:52 -0800

    created list of course sequence by student as string of sequences

--- seqcrs/data/create_sequences.py
+++ seqcrs/data/create_sequences.py
@@ -6,9 +6,10 @@ import re
 import sys
 from pathlib import Path
 import sqlite3
+from sklearn.feature_extraction.text import CountVectorizer
 
 this_file_path = os.path.abspath(__file__)
-project_root = os.path.split(this_file_path)[0]
+project_root = os.path.split(os.path.split(os.path.split(this_file_path)[0])[0])[0]
  
 data_path = os.path.join(project_root, "data") + '/'
 
@@ -40,21 +41,39 @@ single_case_df = df_courses[single_case]
 df_sorted = df_courses.sort_values(['CourseTitle'], ascending=True).groupby(['ResearchID'], sort=False)\
     .apply(lambda x: x.sort_values(['TermEndDate'], ascending = True)).reset_index(drop=True)
 
-double_case = ['#######', '#######']
-double_case_df = df_sorted[df_sorted.ResearchID.isin(double_case)] 
+sub_cases_df = df_sorted[0:100]
 
-failed_courses = double_case_df['CreditsEarned'].astype(float) > 0 
-double_case_df =  double_case_df[failed_courses].reset_index(drop=True)
+failed_courses = sub_cases_df['CreditsEarned'].astype(float) > 0 
+sub_cases_df =  sub_cases_df[failed_courses].reset_index(drop=True)
 
 columns = ['ResearchID', 'CourseTitle']
-pivot_df = double_case_df[columns]
-
-test = pivot_df.groupby('ResearchID')['CourseTitle'].apply(lambda df: df.reset_index(drop=True)).unstack()
+pivot_df = sub_cases_df[columns]
 
 # Most promising method so far!
-pivot_df.groupby('ResearchID').agg({'CourseTitle':lambda x: list(x)})
+course_lists = pivot_df.groupby('ResearchID').agg({'CourseTitle':lambda x: list(x)})
 # Clean up list of sequences
 # 1. Replace spaces + / with underscores
 # 2. Lower Case
+course_seq_ls = course_lists['CourseTitle'].to_list() 
+
+# WIP: make function replace parentheses and others 
+test = [[x.lower() for x in l] for l in course_seq_ls]
+test = [[x.replace(' ', '_').lower() for x in l] for l in course_seq_ls]
+test = [[x.replace('/', '_') for x in l] for l in test]
+
+test = [[" ".join(w for w in l)] for l in test]
+test = [x for sublist in test for x in sublist] # courses as sequences flattened lists
 
 # Check sequence distribution for Renton  
+def get_top_n_courses(corpus, n=None):
+    """
+    List the top n words in a vocabulary according to occurrence in a text corpus.
+    """
+    vec = CountVectorizer().fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+get_top_n_courses(test)
