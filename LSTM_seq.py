d3e75b154274df17787d21ba98aeec4b6436eab5
Jose Hernandez <jmhernandeze@gmail.com>
2021-03-29 13:29:36 -0700

    LSTM base model good WIP:create model func/class

--- LSTM_seq.py
+++ LSTM_seq.py
@@ -94,8 +94,7 @@ model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train, epochs=20,
-                    validation_data=(x_val, y_val), 
-                    validation_steps=30)
+                    validation_data=(x_val, y_val))
 
 test_loss, test_acc = model.evaluate(x_val, y_val)
 
@@ -118,34 +117,33 @@ plot_graphs(history, 'loss')
 plt.ylim(0,None)
 
 # WIP PREDICTIONS
-predictions = model.predict(np.array([sample_courses]))
+# predictions = model.predict(np.array([sample_courses])) 
 
 # Another layer
-model = tf.keras.Sequential([
+model_multi = tf.keras.Sequential([
     tf.keras.layers.Embedding(VOCAB_SIZE, embedding_dim, mask_zero=True),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(100)),
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dropout(dropout),
     tf.keras.layers.Dense(1)
 ])
 
-model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+model_multi.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               optimizer=tf.keras.optimizers.Adam(1e-4),
               metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, epochs=20,
-                    validation_data=(x_val, y_val), 
-                    validation_steps=30)
+history = model_multi.fit(x_train, y_train, epochs=20,
+                    validation_data=(x_val, y_val))
 
-test_loss, test_acc = model.evaluate(x_val, y_val)
+test_loss, test_acc = model_multi.evaluate(x_val, y_val)
 
 print('Test Loss: {}'.format(test_loss))
 print('Test Accuracy: {}'.format(test_acc))
 
 plt.figure(figsize=(16,8))
 plt.subplot(1,2,1)
-plot_graphs(history, 'acc')
+plot_graphs(history, 'accuracy')
 plt.ylim(None,1)
 plt.subplot(1,2,2)
 plot_graphs(history, 'loss')
