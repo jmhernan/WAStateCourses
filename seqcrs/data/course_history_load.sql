0fe9b9fea32b6ffb050e3a326b80db9a1391e241
Jose Hernandez <jmhernandeze@gmail.com>
2021-04-27 15:00:13 -0700

    sql script to order sequences

--- seqcrs/data/course_history_load.sql
+++ seqcrs/data/course_history_load.sql
@@ -1,3 +1,17 @@
-/* Load Course History File */
-SELECT ResearchID, Term, CourseTitle, GradeLevelWhenCourseTaken
-FROM hsCourses;
+-- Load Course History File
+
+/* Working with the large student grade history file
+    before loading to python */
+
+/* WIP: 
+    Steps:
+    1. Group by grade level and Order by grade level 9,10,11,12
+    2. Order by courseTitle within grade
+*/
+
+-- Change Gradelevel to int 
+
+SELECT ResearchID, CourseTitle, GradeLevelWhenCourseTaken
+FROM hsCourses
+ORDER BY GradeLevelWhenCourseTaken
+LIMIT 10;
