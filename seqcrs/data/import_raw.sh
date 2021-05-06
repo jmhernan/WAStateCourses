2ec6f993118a60f041e3d93c09af91bd0c4a6c34
Jose Hernandez <jmhernandeze@gmail.com>
2021-05-06 10:08:48 -0700

    modification to shell script sor compatibility

--- seqcrs/data/import_raw.sh
+++ seqcrs/data/import_raw.sh
@@ -3,14 +3,14 @@
 
 export PROJECTDIR=$HOME/source/WAStateCourses  
 export CODEDIR=$PROJECTDIR/seqcrs
-export CCER_DATA_DUMP=$HOME/data
+export CCER_DATA_DUMP=$HOME/data/cadrs_collaboration_data_update
 export DB_PATH=$PROJECTDIR/data/ccer_data.db
 
 rm -f DB_PATH
 
 for file in $CCER_DATA_DUMP/*.txt
 do
-    (echo .seperator |; echo .import ${file} $(basename ${file} | cut -d. -f1)) | sqlite3 $DB_PATH
+    (echo .seperator | .import ${file} $(basename ${file} | cut -d. -f1)) | sqlite3 $DB_PATH
 done
 
 sqlite3 $DB_PATH '.read '${CODEDIR}'/data/create_cohort.sql'
@@ -24,3 +24,5 @@ SELECT COUNT(*) FROM Dim_School;
 SELECT COUNT(*) FROM hsCourses;
 SELECT COUNT(*) FROM postSecDems;
 SQL
+
+.separator "|"
