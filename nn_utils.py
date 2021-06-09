6e69a4406d31f540e8c6d02d8f15d553cbebd622
Jose Hernandez <jmhernandeze@gmail.com>
2021-06-09 11:32:46 -0700

    some minor documetation added

--- nn_utils.py
+++ nn_utils.py
@@ -3,18 +3,55 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-# WIP: Incorporate stop rules using callback limit...maybe once accuracy doesn't change by
+# WIP: Incorporate stop rules using callback limit...maybe once accuracy 
+# doesn't change by
 # by some factor. 
 class callbackLimit():
     pass
 
-def lstm_model_build(vocab_size, embedding_dim=64, dropout=.50, nodes = 100, embedding_training=True):
-    model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim, mask_zero=True),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(nodes)),
-    tf.keras.layers.Dense(embedding_dim, activation='relu'),
-    tf.keras.layers.Dropout(dropout),
-    tf.keras.layers.Dense(1, activation='sigmoid')])
+def lstm_model_build(vocab_size, embedding_dim=64, dropout=.50, nodes = 100, 
+                     embedding_matrix=None):
+    """ Builds and returns LSTM model
+    Parameters:
+    ----------
+    vocab_size: int
+        the size of your corpus vocabulary after cleanup, usually we use 
+        (size_of_vocab + 1)
+    
+    embedding_dim: int
+        this is the size of your embedding space per word, if you have a
+        pre-trained embedding matrix then you have to make sure the embedding
+        dimensions match. Defaults to 64
+    
+    dropout: float
+    
+    nodes: int
+
+    embedding_matrix: numpy narray
+        A word by word embedding matrix with weights for the whole vocabulary
+        with dimension (vocabulary size x embedding vector). Defaults to None
+    
+    Returns:
+    -------
+    Tensorflow model object
+    """
+    if embedding_matrix is not None:
+        model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(input_dim = vocab_size, 
+                                  output_dim  = embedding_dim, 
+                                  embeddings_initializer = tf.keras.initializers.Constant(embedding_matrix), 
+                                  trainable = False),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(nodes)),
+        tf.keras.layers.Dense(embedding_dim, activation='relu'),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Dense(1, activation='sigmoid')])
+    else:
+        model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, mask_zero=True),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(nodes)),
+        tf.keras.layers.Dense(embedding_dim, activation='relu'),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Dense(1, activation='sigmoid')])
     return model
 
 
